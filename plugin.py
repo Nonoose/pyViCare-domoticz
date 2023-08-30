ba812781029314e5fb9e9da7d413ab57973f1e84
wimmme <wimmme@gmail.com>
2023-08-30 13:29:36 +0200

    First working version
    This is just a starting point. It stores the outside temperature in a Domitcz device. 
    Viessmann has put the most interesting features from the API (FuelCell) in their most expensive non-free subscription, bummer :-/

--- plugin.py
+++ plugin.py
@@ -1,50 +1,78 @@
-# pyViCare Domoticz implementation
-#
-# Author: wimmme
-#
 """
-<plugin key="pyViVare" name="PyViCare Domoticz implementation" author="wimmme" version="1.0.0" externallink="https://github.com/somm15/PyViCare">
+<plugin key="pyViCare-domoticz" name="pyViCare-domoticz Viessmann API Plugin" author="wimmme" version="1.0.0" wikilink="https://www.domoticz.com/wiki/Developing_a_Python_plugin" externallink="https://github.com/somm15/PyViCare">
     <description>
-        <h2>PyViCare Domoticz implementation</h2><br/>
-         Implements access to Viessmann devices by using the official API from the Viessmann Developer Portal
-        <h3>Details</h3>
-        For more info take a look at https://github.com/somm15/PyViCare
+        Domoticz plugin to access the Viessmann API using pyViCare library.
+        All credits to the makes of pyViCare, this plugin is just a wrappers for Domoticz
     </description>
     <params>
-        <param field="Username" label="Username" width="300px" required="false" default=""/>
-        <param field="Password" label="Password" width="300px" required="false" default="" password="true"/>
-        <param field="Client_id" label="Client_id" width="300px" required="false" default=""/>
-        <param field="Mode6" label="Debug" width="150px">
+        <param field="Username" label="Viessmann Email" width="300px" required="true"/>
+        <param field="Password" label="Viessmann Password" width="300px" required="true" password="true"/>
+        <param field="Mode1" label="Viessmann API client ID" width="300px" required="true"/>
+        <param field="Mode2" label="Viessmann update interval" width="300px" required="true" default="60"/>
+        <param field="Mode6" label="Debug" width="75px">
             <options>
-                <option label="None" value="0"  default="true" />
-                <option label="Python Only" value="2"/>
-                <option label="Basic Debugging" value="62"/>
-                <option label="Basic+Messages" value="126"/>
-                <option label="Queue" value="128"/>
-                <option label="Connections Only" value="16"/>
-                <option label="Connections+Queue" value="144"/>
-                <option label="All" value="-1"/>
+                <option label="True" value="Debug"/>
+                <option label="False" value="Normal" default="true" />
             </options>
         </param>
     </params>
 </plugin>
 """
-import DomoticzEx as Domoticz
+import Domoticz
+import os
+import sys
+import logging
+from PyViCare.PyViCare import PyViCare
 
 class BasePlugin:
     enabled = False
+
     def __init__(self):
-        #self.var = 123
-        return
+        self.vicare = None
+        self.client_id = ""
+        self.Username = ""
+        self.Password = ""
+        self.update_interval = ""
+        self.DEBUG = ""
+       
 
     def onStart(self):
         Domoticz.Log("onStart called")
-        if Parameters["Mode6"] != "0":
-            Domoticz.Debugging(int(Parameters["Mode6"]))
-            DumpConfigToLog()
+        self.debugging = Parameters["Mode6"]
+        self.update_interval = int(Parameters["Mode2"])
 
-        #if (not "Dimmer" in Devices):
-        #    Domoticz.Unit(Name="Dimmer", Unit=2, TypeName="Dimmer", DeviceID="Dimmer").Create()
+        self.client_id = Parameters["Mode1"].strip()
+        self.Username = Parameters["Username"].strip()
+        self.Password = Parameters["Password"].strip()
+        self.DEBUG = Parameters["Mode6"]
+
+        if self.DEBUG == "Debug":
+            Domoticz.Debugging(1)
+
+        if len(self.Username) == 0 or len(self.Password) == 0 or len(self.client_id) == 0:
+            Domoticz.Log("Please provide Viessmann Email, Password and client_ID in the plugin settings.")
+            return 
+
+        try:
+            self.vicare = PyViCare()
+            self.vicare.initWithCredentials(self.Username, self.Password, self.client_id, os.path.join(os.path.expanduser('~'),"token.save"))
+
+            device = self.vicare.devices[0]
+
+            Domoticz.Log("Model: " + device.getModel())
+            Domoticz.Log("Status: " + "Online" if device.isOnline() else "Offline") 
+          
+            self.enabled = True
+
+            if len(Devices) == 0:
+                Domoticz.Device(Name="BuitenTemperatuurVoordeur", Unit=1, TypeName="Temperature", Used=1).Create()
+
+            Domoticz.Heartbeat(self.update_interval)
+            
+        except Exception as e:
+            Domoticz.Log("Failed to initialize pyViCare: {}".format(str(e)))
+            self = None
+       
 
     def onStop(self):
         Domoticz.Log("onStop called")
@@ -55,17 +83,48 @@ class BasePlugin:
     def onMessage(self, Connection, Data):
         Domoticz.Log("onMessage called")
 
-    def onCommand(self, DeviceID, Unit, Command, Level, Color):
-        Domoticz.Log("onCommand called for Device " + str(DeviceID) + " Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level))
+    def onCommand(self, Unit, Command, Level, Hue):
+        Domoticz.Log("onCommand called")
 
     def onNotification(self, Name, Subject, Text, Status, Priority, Sound, ImageFile):
-        Domoticz.Log("Notification: " + Name + "," + Subject + "," + Text + "," + Status + "," + str(Priority) + "," + Sound + "," + ImageFile)
+        Domoticz.Log("onNotification called")
 
     def onDisconnect(self, Connection):
         Domoticz.Log("onDisconnect called")
 
     def onHeartbeat(self):
-        Domoticz.Log("onHeartbeat called")
+        if self.vicare is not None:
+            try:
+                # Fetch the data from Viessmann API using pyViCare
+                device = self.vicare.devices[0] 
+                vitovalor = device.asAutoDetectDevice()
+                # circuit = vitovalor.circuits[0]
+                # burner = vitovalor.burners[0]
+
+                outside_temp = vitovalor.getOutsideTemperature()
+                # inflow_temp = circuit.getSupplyTemperature()
+                # active_mode = circuit.getActiveProgram()
+                # burner_active = burner.getActive()
+
+                # print("Temp warmwater config: " + str(t.getDomesticHotWaterConfiguredTemperature()))
+                # print("Temp Opslag nu: " + str(t.getDomesticHotWaterStorageTemperature()))
+
+                Domoticz.Log("Outside temperature: " + str(outside_temp))
+                Devices[1].Update(nValue=0,sValue=str(outside_temp))
+                
+                # Domoticz.Log("Inflow temperature: " + str(inflow_temp))
+                # Domoticz.Log("Active mode: " + active_mode)
+                # Domoticz.Log("Burner active: " + str(burner_active))
+
+
+            except Exception as e:
+                Domoticz.Log("Error fetching data from Viessmann API: {}".format(str(e)))
+        else:
+            Domoticz.Log("pyViCare not initialized. Please check your Viessmann Username and Password in plugin settings.")
+
+# Define Domoticz log function
+def LogMessage(message):
+    Domoticz.Log(message)
 
 global _plugin
 _plugin = BasePlugin()
@@ -86,9 +145,9 @@ def onMessage(Connection, Data):
     global _plugin
     _plugin.onMessage(Connection, Data)
 
-def onCommand(DeviceID, Unit, Command, Level, Color):
+def onCommand(Unit, Command, Level, Hue):
     global _plugin
-    _plugin.onCommand(DeviceID, Unit, Command, Level, Color)
+    _plugin.onCommand(Unit, Command, Level, Hue)
 
 def onNotification(Name, Subject, Text, Status, Priority, Sound, ImageFile):
     global _plugin
@@ -102,21 +161,5 @@ def onHeartbeat():
     global _plugin
     _plugin.onHeartbeat()
 
-# Generic helper functions
-def DumpConfigToLog():
-    for x in Parameters:
-        if Parameters[x] != "":
-            Domoticz.Debug( "'" + x + "':'" + str(Parameters[x]) + "'")
-    Domoticz.Debug("Device count: " + str(len(Devices)))
-    for DeviceName in Devices:
-        Device = Devices[DeviceName]
-        Domoticz.Debug("Device ID:       '" + str(Device.DeviceID) + "'")
-        Domoticz.Debug("--->Unit Count:      '" + str(len(Device.Units)) + "'")
-        for UnitNo in Device.Units:
-            Unit = Device.Units[UnitNo]
-            Domoticz.Debug("--->Unit:           " + str(UnitNo))
-            Domoticz.Debug("--->Unit Name:     '" + Unit.Name + "'")
-            Domoticz.Debug("--->Unit nValue:    " + str(Unit.nValue))
-            Domoticz.Debug("--->Unit sValue:   '" + Unit.sValue + "'")
-            Domoticz.Debug("--->Unit LastLevel: " + str(Unit.LastLevel))
-    return
+def LogMessage(message):
+    Domoticz.Log(message)
